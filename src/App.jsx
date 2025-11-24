0c0270038c5c9c36d7f85bd4aa55eb031be46853
anitaorji <orjianita15@gmail.com>
2025-11-23 17:21:01 -0800

    Worked on footer page and reviews page. Not yet done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect} from "react";
+import aPgination from "./components/pagination";
+import 'App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Property() { 
+    const[properties,setProperties] = useState([]);
+    const[currentPageInfo,setCurrentPageInfo] = useState(
+      {pageNumber:1, pageSize:1})
+    
+      const fetchPropertiesData = async (page = 1) => {
+        const response = await fetch (`http://localhost:5044/api/Products?pageNumber=${page}&pageSize=5`)
+        
+        const data = await response.json();
+        setProperties(data.properties);
+        setCurrentPageInfo({
+          pageNumber: data.pageNumber,
+          pageSize: data.pageSize,
+      
+        })
+      };
+        useEffect(() => {
+        try {
+          fetchPropertiesData();
+        }  
+        catch (error) {
+            console.error("Error fetching properties:", error);
+        }
+      }, []);
 
-  return (
-    <>
-    </>
-  )
-}
+        
 
-export default App
+        return (
+            <>
+    <div className="pagination">
+      <button></button>
+      </div>
+      </>
+        )
+    }
+
+    export default Property
